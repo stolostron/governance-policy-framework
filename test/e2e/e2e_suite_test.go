9a18631991a54fb95b3c78bb66e3f1ddc0ada7f0
Yu Cao <ycao@redhat.com>
2020-05-28 16:12:02 -0400

    check cert policy

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -32,6 +32,7 @@ var (
 	clientManaged          kubernetes.Interface
 	clientManagedDynamic   dynamic.Interface
 	gvrPolicy              schema.GroupVersionResource
+	gvrCertPolicy          schema.GroupVersionResource
 	gvrConfigurationPolicy schema.GroupVersionResource
 	gvrPlacementBinding    schema.GroupVersionResource
 	gvrPlacementRule       schema.GroupVersionResource
@@ -61,6 +62,7 @@ var _ = BeforeSuite(func() {
 	By("Setup Hub client")
 	gvrPolicy = schema.GroupVersionResource{Group: "policies.open-cluster-management.io", Version: "v1", Resource: "policies"}
 	gvrConfigurationPolicy = schema.GroupVersionResource{Group: "policies.open-cluster-management.io", Version: "v1", Resource: "configurationpolicies"}
+	gvrCertPolicy = schema.GroupVersionResource{Group: "policies.open-cluster-management.io", Version: "v1", Resource: "certificatepolicies"}
 	gvrPlacementBinding = schema.GroupVersionResource{Group: "policies.open-cluster-management.io", Version: "v1", Resource: "placementbindings"}
 	gvrPlacementRule = schema.GroupVersionResource{Group: "apps.open-cluster-management.io", Version: "v1", Resource: "placementrules"}
 	gvrRole = schema.GroupVersionResource{Group: "rbac.authorization.k8s.io", Version: "v1", Resource: "roles"}
