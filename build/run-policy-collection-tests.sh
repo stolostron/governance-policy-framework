1f4f6e8121fc9bd25a7081d6ec0b9a577a826dff
Yu Cao <ycao@redhat.com>
2020-08-12 15:33:02 -0400

    test

--- build/run-policy-collection-tests.sh
+++ build/run-policy-collection-tests.sh
@@ -11,3 +11,25 @@ git clone git@github.com:open-cluster-management/policy-collection.git
 cd policy-collection/deploy
 
 ./deploy.sh https://github.com/open-cluster-management/policy-collection.git stable policies
+
+COMPLETE=1
+for i in {1..20}; do
+    clear
+    ROOT_POLICIES=$(oc get policies -n policies | tail -n +2 | wc -l | tr -d '[:space:]')
+    TOTAL_POLICIES=$(oc get policies -A | tail -n +2 | wc -l | tr -d '[:space:]')
+    if [ $ROOT_POLICIES -eq 10 ] && [ $TOTAL_POLICIES -eq 20 ]; then
+        COMPLETE=0
+        break
+    fi
+    echo
+    echo "Number of expected Policies : 10/20"
+    echo "Number of expected Policies : $ROOT_POLICIES/$TOTAL_POLICIES"
+    sleep 10
+done
+if [ $COMPLETE -eq 1 ]; then
+    echo "Number of expected Policies : 10/20"
+    echo "Number of expected Policies : $ROOT_POLICIES/$TOTAL_POLICIES"
+    exit 1
+fi
+
+exit 0
