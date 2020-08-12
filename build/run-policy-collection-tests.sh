b199b33d9f004911854ab812217d2fdaeeb22169
Yu Cao <ycao@redhat.com>
2020-08-12 15:35:43 -0400

    install kubectl

--- build/run-policy-collection-tests.sh
+++ build/run-policy-collection-tests.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if ! which kubectl > /dev/null; then
+    echo "installing kubectl"
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+fi
+
 echo "Login hub"
 make oc/login
 
