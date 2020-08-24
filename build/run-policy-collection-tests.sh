63103ad929b9344e60cc09ab535c9701d88db283
Yu Cao <ycao@redhat.com>
2020-08-24 15:54:00 -0400

    fix ns

--- build/run-policy-collection-tests.sh
+++ build/run-policy-collection-tests.sh
@@ -10,23 +10,23 @@ fi
 echo "Login hub"
 make oc/login
 
-kubectl create ns e2e || true
+kubectl create ns policy-collection-e2e || true
 
 git clone https://github.com/open-cluster-management/policy-collection.git
 cd policy-collection/deploy
 
-./deploy.sh https://github.com/open-cluster-management/policy-collection.git stable e2e-policies
+./deploy.sh https://github.com/open-cluster-management/policy-collection.git stable policy-collection-e2e
 
 function cleanup {
-    kubectl delete subscriptions -n e2e --all || true
-    kubectl delete channels -n e2e --all || true
-    kubectl delete policies -n e2e --all || true
+    kubectl delete subscriptions -n policy-collection-e2e --all || true
+    kubectl delete channels -n policy-collection-e2e --all || true
+    kubectl delete policies -n policy-collection-e2e --all || true
 }
 
 COMPLETE=1
 for i in {1..20}; do
-    ROOT_POLICIES=$(kubectl get policies -n e2e-policies | tail -n +2 | wc -l | tr -d '[:space:]')
-    TOTAL_POLICIES=$(kubectl get policies -A | grep e2e-policies | wc -l | tr -d '[:space:]')
+    ROOT_POLICIES=$(kubectl get policies -n policy-collection-e2e | tail -n +2 | wc -l | tr -d '[:space:]')
+    TOTAL_POLICIES=$(kubectl get policies -A | grep e2e | wc -l | tr -d '[:space:]')
     echo "Number of expected Policies : 10/20"
     echo "Number of actual Policies : $ROOT_POLICIES/$TOTAL_POLICIES"
     if [ $TOTAL_POLICIES -eq 20 ]; then
