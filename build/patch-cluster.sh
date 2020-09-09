b7172c9ce01941eaa5c02b64695dbbf369f98b32
Yu Cao <ycao@redhat.com>
2020-09-09 10:48:41 -0400

    add wait for

--- build/patch-cluster.sh
+++ build/patch-cluster.sh
@@ -2,14 +2,20 @@
 
 set -e
 
-echo "Login hub"
-export OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL
-export OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS
-make oc/login
+# echo "Login hub"
+# export OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL
+# export OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS
+# make oc/login
 
 oc delete pod -l app=console-header -A
 oc delete pod -l app=grc -A
 oc delete pod -l component=governance -A
 oc delete pod -l app=klusterlet-addon-iampolicyctrl -A
 oc delete pod -l app=cert-policy-controller -A
+
+./build/wait_for.sh pod -l app=console-header -A
+./build/wait_for.sh pod -l app=grc -A
+./build/wait_for.sh pod -l component=governance -A
+./build/wait_for.sh pod -l app=klusterlet-addon-iampolicyctrl -A
+./build/wait_for.sh pod -l app=cert-policy-controller -A
 
