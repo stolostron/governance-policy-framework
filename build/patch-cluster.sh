bd06431146a1fcd3fb571e5c4ba56202e6153e3a
Yu Cao <ycao@redhat.com>
2020-09-09 10:33:45 -0400

    Patch cluster with latest dev images before each run

---
+++ build/patch-cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo "Login hub"
+export OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL
+export OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS
+make oc/login
+
+oc delete pod -l app=grc -A
+oc delete pod -l component=governance -A
+oc delete pod -l app=klusterlet-addon-iampolicyctrl -A
+oc delete pod -l app=cert-policy-controller -A
+
