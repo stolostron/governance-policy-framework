886f489f4fe1ab6a82a73908a0275036eb8d80e2
Yu Cao <ycao@redhat.com>
2020-09-09 12:55:00 -0400

    Patch cluster with latest dev images before each run (#28)

--- build/ff.sh
+++ build/ff.sh
@@ -7,10 +7,14 @@ body='{
 "branch":"master"
 }}'
 
-curl -s -X POST \
-   -H "Content-Type: application/json" \
-   -H "Accept: application/json" \
-   -H "Travis-API-Version: 3" \
-   -H "Authorization: token $TRAVIS_TOKEN" \
-   -d "$body" \
-   https://api.travis-ci.com/repo/open-cluster-management%2Fpolicy-grc-squad/requests
+if [ $FF == 'true' ]; then
+   curl -s -X POST \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/json" \
+      -H "Travis-API-Version: 3" \
+      -H "Authorization: token $TRAVIS_TOKEN" \
+      -d "$body" \
+      https://api.travis-ci.com/repo/open-cluster-management%2Fpolicy-grc-squad/requests
+else
+  echo 'skipping fast forward'
+fi
