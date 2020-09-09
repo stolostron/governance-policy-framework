dd153f656d7b9c6e066683459150e1de8133c80c
Yu Cao <ycao@redhat.com>
2020-09-09 10:37:14 -0400

    update header

--- build/patch-cluster.sh
+++ build/patch-cluster.sh
@@ -7,6 +7,7 @@ export OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL
 export OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS
 make oc/login
 
+oc delete pod -l app=console-header -A
 oc delete pod -l app=grc -A
 oc delete pod -l component=governance -A
 oc delete pod -l app=klusterlet-addon-iampolicyctrl -A
