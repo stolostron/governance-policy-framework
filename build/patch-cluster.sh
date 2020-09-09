b40365cfb2832a89f26eb48d17d17e02a6a73c23
Yu Cao <ycao@redhat.com>
2020-09-09 10:48:50 -0400

    revert

--- build/patch-cluster.sh
+++ build/patch-cluster.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-# echo "Login hub"
-# export OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL
-# export OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS
-# make oc/login
+echo "Login hub"
+export OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL
+export OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS
+make oc/login
 
 oc delete pod -l app=console-header -A
 oc delete pod -l app=grc -A
