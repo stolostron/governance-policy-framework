e579f5d25e7ccec05abcaeffca1777eff10a45de
Yu Cao <ycao@redhat.com>
2020-09-17 15:51:30 -0400

    fix syntax

--- build/run-e2e-tests-ui.sh
+++ build/run-e2e-tests-ui.sh
@@ -20,7 +20,7 @@ make oc/command
 
 echo "Login managed to clean up"
 export OC_CLUSTER_URL=${OC_MANAGED_CLUSTER_URL:-${OC_HUB_CLUSTER_URL}}
-export OC_CLUSTER_PASS=${OC_MANAGED_CLUSTER_PASS:-${OC_HUB_CLUSTER_PASS:}}
+export OC_CLUSTER_PASS=${OC_MANAGED_CLUSTER_PASS:-${OC_HUB_CLUSTER_PASS}}
 make oc/login
 oc delete pod --all -n default || true
 oc delete issuers.cert-manager.io -l e2e=true -n default || true
