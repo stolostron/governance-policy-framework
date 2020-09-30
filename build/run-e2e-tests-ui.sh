262d60680ef2a1a88de31d46d2b1e0a96fb70c70
Yu Cao <ycao@redhat.com>
2020-09-30 16:12:22 -0400

    skip install cert manager if already installed

--- build/run-e2e-tests-ui.sh
+++ build/run-e2e-tests-ui.sh
@@ -28,8 +28,12 @@ oc delete certificates.cert-manager.io -l e2e=true -n default || true
 oc delete secret -n default rsa-ca-sample-secret || true # in case secrets are empty
 oc delete clusterrolebinding -l e2e=true || true
 
-echo "Install cert manager on managed"
-oc apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.1/cert-manager.yaml
+if [ $(oc get ns cert-manager | grep Active | wc -l | tr -d '[:space:]') -eq 1 ]; then
+    echo "Cert manager alreayd installed"
+else 
+    echo "Install cert manager on managed"
+    oc apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.1/cert-manager.yaml
+fi
 
 echo "Logout"
 export OC_COMMAND=logout
