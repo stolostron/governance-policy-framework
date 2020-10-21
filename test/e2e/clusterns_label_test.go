433dc0f02771cbc6fc9f29e0354b9b11aea9ce2d
Yu Cao <ycao@redhat.com>
2020-10-21 13:59:09 -0400

    add test case to check for cluster ns label (#35)

---
+++ test/e2e/clusterns_label_test.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Red Hat, Inc.
+
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("Test cluster ns creation", func() {
+	It("Should contain label on cluster ns", func() {
+		ns, err := clientManaged.CoreV1().Namespaces().Get(clusterNamespace, metav1.GetOptions{})
+		Expect(err).To(BeNil())
+		Expect(ns.GetLabels()["policy.open-cluster-management.io/isClusterNamespace"]).To(Equal("true"))
+	})
+})
