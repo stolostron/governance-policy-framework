3d30e0b0db00af4864f83345da31547288fbc2ef
Yu Cao <ycao@redhat.com>
2020-12-02 19:38:58 -0500

    clean up

--- test/e2e/gatekeeper_test.go
+++ test/e2e/gatekeeper_test.go
@@ -73,7 +73,7 @@ var _ = Describe("Test gatekeeper", func() {
 		It("should create gatekeeper pods on managed cluster", func() {
 			By("Checking number of pods in gatekeeper-system ns")
 			podList := utils.ListWithTimeoutByNamespace(clientManagedDynamic, gvrPod, metav1.ListOptions{}, "gatekeeper-system", 6, true, 240)
-			fmt.Printf("%+v\n", podList)
+			// fmt.Printf("%+v\n", podList)
 		})
 	})
 	Describe("Test gatekeeper policy creation", func() {
