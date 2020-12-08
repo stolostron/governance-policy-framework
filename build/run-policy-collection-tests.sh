74c3b4bf43eaf2d1c9943590b7ba5fad59b1440f
Yu Cao <ycao@redhat.com>
2020-12-08 14:44:38 -0500

    install ginko

--- build/run-policy-collection-tests.sh
+++ build/run-policy-collection-tests.sh
@@ -6,6 +6,9 @@ if ! which kubectl > /dev/null; then
     echo "installing kubectl"
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 fi
+echo "Installing ginkgo ..."
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega/...
 
 echo "Login hub"
 export OC_CLUSTER_URL=${OC_HUB_CLUSTER_URL:-https://api.chocolate.dev08.red-chesterfield.com:6443}
