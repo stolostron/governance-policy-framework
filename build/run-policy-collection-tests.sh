c5341f61be08e472a9a54a68dcc3a210c29404ac
Yu Cao <ycao@redhat.com>
2020-12-14 10:56:27 -0500

    test

--- build/run-policy-collection-tests.sh
+++ build/run-policy-collection-tests.sh
@@ -22,8 +22,8 @@ export OC_CLUSTER_USER=${OC_CLUSTER_USER:-kubeadmin}
 export OC_CLUSTER_PASS=${OC_HUB_CLUSTER_PASS:-${OC_CLUSTER_PASS}}
 oc login ${OC_CLUSTER_URL} --insecure-skip-tls-verify=true -u ${OC_CLUSTER_USER} -p ${OC_CLUSTER_PASS}
 
-export HUB_KUBECONFIG=`echo ~/.kube/config`
-export MANAGED_KUBECONFIG=`echo ~/.kube/config`
+export HUB_KUBECONFIG=${HUB_KUBECONFIG:-`echo ~/.kube/config`}
+export MANAGED_KUBECONFIG=${MANAGED_KUBECONFIG:-`echo ~/.kube/config`}
 export MANAGED_CLUSTER_NAME=${MANAGED_CLUSTER_NAME:-"local-cluster"}
 
 printenv
