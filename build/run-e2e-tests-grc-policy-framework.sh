b412e12ce0b62943cc824b4e7791fa03f9128444
Yu Cao <ycao@redhat.com>
2020-12-14 09:44:18 -0500

    test

---
+++ build/run-e2e-tests-grc-policy-framework.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+TEST_IMAGE=$1
+
+echo $1
+
+printenv
+
+docker run --volume $(pwd)/results:/go/src/github.com/open-cluster-management/governance-policy-framework/test-output \
+    --env OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL \
+    --env OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS \
+    --env OC_CLUSTER_USER=$OC_CLUSTER_USER \
+    $DOCKER_URI
