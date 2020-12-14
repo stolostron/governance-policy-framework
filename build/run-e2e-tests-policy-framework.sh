ad1a91057ff00cb94d8f2d0119ac6935bd23c7a1
Yu Cao <ycao@redhat.com>
2020-12-14 10:58:22 -0500

    test

--- build/run-e2e-tests-policy-framework.sh
+++ build/run-e2e-tests-policy-framework.sh
@@ -3,10 +3,23 @@
 set -e
 TEST_IMAGE_URI=$1
 
+echo "Login hub"
+export OC_CLUSTER_URL=${OC_HUB_CLUSTER_URL:-${OC_CLUSTER_URL}}
+export OC_CLUSTER_USER=${OC_CLUSTER_USER:-kubeadmin}
+export OC_CLUSTER_PASS=${OC_HUB_CLUSTER_PASS:-${OC_CLUSTER_PASS}}
+oc login ${OC_CLUSTER_URL} --insecure-skip-tls-verify=true -u ${OC_CLUSTER_USER} -p ${OC_CLUSTER_PASS}
+
+export HUB_KUBECONFIG=${HUB_KUBECONFIG:-`echo ~/.kube/config`}
+export MANAGED_KUBECONFIG=${MANAGED_KUBECONFIG:-`echo ~/.kube/config`}
+export MANAGED_CLUSTER_NAME=${MANAGED_CLUSTER_NAME:-"local-cluster"}
+
 printenv
 
 docker run --volume $(pwd)/results:/go/src/github.com/open-cluster-management/governance-policy-framework/test-output \
     --env OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL \
     --env OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS \
     --env OC_CLUSTER_USER=$OC_CLUSTER_USER \
+    --env HUB_KUBECONFIG=$HUB_KUBECONFIG \
+    --env MANAGED_KUBECONFIG=$MANAGED_KUBECONFIG \
+    --enc MANAGED_CLUSTER_NAME=$MANAGED_CLUSTER_NAME
     $TEST_IMAGE_URI
--- build/run-policy-collection-tests.sh
+++ build/run-policy-collection-tests.sh
@@ -16,15 +16,4 @@ echo "Installing ginkgo ..."
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega/...
 
-echo "Login hub"
-export OC_CLUSTER_URL=${OC_HUB_CLUSTER_URL:-${OC_CLUSTER_URL}}
-export OC_CLUSTER_USER=${OC_CLUSTER_USER:-kubeadmin}
-export OC_CLUSTER_PASS=${OC_HUB_CLUSTER_PASS:-${OC_CLUSTER_PASS}}
-oc login ${OC_CLUSTER_URL} --insecure-skip-tls-verify=true -u ${OC_CLUSTER_USER} -p ${OC_CLUSTER_PASS}
-
-export HUB_KUBECONFIG=${HUB_KUBECONFIG:-`echo ~/.kube/config`}
-export MANAGED_KUBECONFIG=${MANAGED_KUBECONFIG:-`echo ~/.kube/config`}
-export MANAGED_CLUSTER_NAME=${MANAGED_CLUSTER_NAME:-"local-cluster"}
-
-printenv
 ginkgo -v --slowSpecThreshold=10 test/policy-collection -- -kubeconfig_hub=$HUB_KUBECONFIG -kubeconfig_managed=$MANAGED_KUBECONFIG -cluster_namespace=$MANAGED_CLUSTER_NAME
