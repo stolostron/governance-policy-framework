6538b9423f1860c35270617e8757305549a59eef
Yu Cao <ycao@redhat.com>
2020-12-15 09:27:16 -0500

    change to oc

--- build/download-clis.sh
+++ build/download-clis.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2020 Red Hat, Inc.
 
-if ! which kubectl > /dev/null; then
+if ! which oc > /dev/null; then
     echo "Installing oc and kubectl clis..."
     mkdir clis-unpacked
     curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.6.6/openshift-client-linux-4.6.6.tar.gz
