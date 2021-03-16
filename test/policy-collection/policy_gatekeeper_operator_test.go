742ecf0f4290289d707ca7a2d6f7a42d4b2ba7d5
Yu Cao <ycao@redhat.com>
2021-03-16 13:51:11 -0400

    remove invalid step

--- test/policy-collection/policy_gatekeeper_operator_test.go
+++ test/policy-collection/policy_gatekeeper_operator_test.go
@@ -299,12 +299,6 @@ var _ = Describe("", func() {
 				fmt.Printf("%v\n", details[2].(map[string]interface{})["history"].([]interface{})[0].(map[string]interface{})["message"])
 				return details[2].(map[string]interface{})["history"].([]interface{})[0].(map[string]interface{})["message"]
 			}, defaultTimeoutSeconds, 1).Should(ContainSubstring("NonCompliant; violation - events found: [e2etestfail."))
-			By("Checking if status for policy template policy-gatekeeper-audit is still compliant")
-			Eventually(func() interface{} {
-				plc := utils.GetWithTimeout(clientHubDynamic, gvrPolicy, userNamespace+"."+GKPolicyName, clusterNamespace, true, defaultTimeoutSeconds)
-				details := plc.Object["status"].(map[string]interface{})["details"].([]interface{})
-				return details[1].(map[string]interface{})["compliant"]
-			}, defaultTimeoutSeconds, 1).Should(Equal("Compliant"))
 		})
 		It("community/policy-gatekeeper-sample should be noncompliant", func() {
 			By("Checking if the status of root policy is noncompliant")
