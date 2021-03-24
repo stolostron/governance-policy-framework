f02719ccd394faac543fd13b5a7c344a1c982b3d
Yu Cao <ycao@redhat.com>
2021-03-24 15:58:41 -0400

    Split UI test and run them in parallel (#84)

---
+++ build/clean-up-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright Contributors to the Open Cluster Management project
+
+
+set -e
+UI_CURRENT_IMAGE=$1
+
+make docker/login
+export DOCKER_URI=quay.io/open-cluster-management/grc-ui-tests:latest-dev
+make docker/pull
+
+docker run --volume $(pwd)/build:/opt/app-root/src/grc-ui/tmp \
+    $DOCKER_URI \
+    cp ./build/{cluster-clean-up.sh,install-cert-manager.sh} tmp/
+
+echo "Login hub to clean up"
+export OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL
+export OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS
+make oc/login
+./build/cluster-clean-up.sh 
+
+./build/install-cert-manager.sh
--- build/run-e2e-tests-ui.sh
+++ build/run-e2e-tests-ui.sh
@@ -9,32 +9,6 @@ make docker/login
 export DOCKER_URI=quay.io/open-cluster-management/grc-ui-tests:latest-dev
 make docker/pull
 
-docker run --volume $(pwd)/build:/opt/app-root/src/grc-ui/tmp \
-    $DOCKER_URI \
-    cp ./build/{cluster-clean-up.sh,install-cert-manager.sh} tmp/
-
-echo "Login hub to clean up"
-export OC_CLUSTER_URL=$OC_HUB_CLUSTER_URL
-export OC_CLUSTER_PASS=$OC_HUB_CLUSTER_PASS
-make oc/login
-./build/cluster-clean-up.sh hub
-
-echo "Logout"
-export OC_COMMAND=logout
-make oc/command
-
-echo "Login managed to clean up"
-export OC_CLUSTER_URL=${OC_MANAGED_CLUSTER_URL:-${OC_HUB_CLUSTER_URL}}
-export OC_CLUSTER_PASS=${OC_MANAGED_CLUSTER_PASS:-${OC_HUB_CLUSTER_PASS}}
-make oc/login
-./build/cluster-clean-up.sh managed
-
-./build/install-cert-manager.sh
-
-echo "Logout"
-export OC_COMMAND=logout
-make oc/command
-
 printenv
 
 docker run --volume $(pwd)/results:/opt/app-root/src/grc-ui/test-output/e2e \
@@ -46,5 +20,7 @@ docker run --volume $(pwd)/results:/opt/app-root/src/grc-ui/test-output/e2e \
     --env PAUSE=0 \
     --env FAIL_FAST=true \
     --env CYPRESS_STANDALONE_TESTSUITE_EXECUTION=FALSE \
+    --env CYPRESS_TAGS_INCLUDE=$CYPRESS_TAGS_INCLUDE \
+    --env CYPRESS_TAGS_EXCLUDE=$CYPRESS_TAGS_EXCLUDE \
     --env MANAGED_CLUSTER_NAME=$MANAGED_CLUSTER_NAME \
     $DOCKER_URI
