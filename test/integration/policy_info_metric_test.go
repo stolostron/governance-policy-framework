9d6885f03789e24a834cb4e607512872e879eba4
Justin Kulikauskas <44813129+JustinKuli@users.noreply.github.com>
2021-08-20 14:58:58 -0400

    Add metrics tests to run on canaries (#142)
    * Add metrics tests to run on canaries
    
    The tests in the propagator itself verifies some of these same things,
    but the tests here use the service and rbac-proxy set up in the Helm
    chart, to verify they are working as expected. One additional test is
    to verify that the metrics can't be seen by unauthorized users.
    
    This utilizes the `oc` cli's ability to expose services as a route, so
    that we can curl directly to the metrics endpoint. One alternative that
    I considered was using a port-forward, but that was difficult to manage.
    
    Refs:
     - https://github.com/open-cluster-management/backlog/issues/14992
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>
    
    * Handle case where there are no policies
    
    If no policies are in the cluster, the metrics endpoint doesn't even
    have the HELP comment. So now the test ensures that there is a policy
    defined (compliance doesn't matter) that can report some metric.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>
    
    * Use net/http instead of shelling to curl
    
    This will allow for some better timeout handling, and more specific
    parsing of the response + response status.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>
    
    * Always try to create the namespace
    
    If the CREATE comes back with an 'already exists' error, then that's
    good.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>
    
    * Pull regex into separate function
    
    This will help document how the regex works, and potentially let it be
    used more easily in other metrics tests in the future.
    
    It would have been nice to be able to take multiple labels, but since
    golang regex does not have lookaround, we would need to know the order
    that the labels will be in.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>
    
    * Fix nitpicks
    
    As mentioned, routeHost is more accurate than routeURL, since it does
    not have the protocol.
    
    For `len(authToken) > 0` vs `authToken != ""`, I think I was worried if
    the input was a nil string. But you can't have a nil string, so that's
    not a problem.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

---
+++ test/integration/policy_info_metric_test.go
@@ -0,0 +1,185 @@
+// Copyright Contributors to the Open Cluster Management project
+
+package integration
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"os/exec"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
+	"github.com/open-cluster-management/governance-policy-framework/test/common"
+	policiesv1 "github.com/open-cluster-management/governance-policy-propagator/pkg/apis/policy/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+const (
+	propagatorMetricsSelector = "component=ocm-policy-propagator"
+	ocmNS                     = "open-cluster-management"
+	metricName                = "policy_governance_info"
+	compliantPolicyYaml       = "../resources/policy_info_metric/compliant.yaml"
+	compliantPolicyName       = "policy-metric-compliant"
+	noncompliantPolicyYaml    = "../resources/policy_info_metric/noncompliant.yaml"
+	noncompliantPolicyName    = "policy-metric-noncompliant"
+)
+
+var routeHost string
+
+var _ = Describe("Test policy_governance_info metric", func() {
+	It("Sets up the metrics service endpoint for tests", func() {
+		By("Ensuring the metrics service exists")
+		svcList, err := clientHub.CoreV1().Services(ocmNS).List(context.TODO(), metav1.ListOptions{LabelSelector: propagatorMetricsSelector})
+		Expect(err).To(BeNil())
+		Expect(len(svcList.Items)).To(Equal(1))
+		metricsSvc := svcList.Items[0]
+
+		By("Checking for an existing metrics route")
+		var routeList *unstructured.UnstructuredList
+		Eventually(func() interface{} {
+			var err error
+			routeList, err = clientHubDynamic.Resource(common.GvrRoute).Namespace(ocmNS).List(context.TODO(), metav1.ListOptions{LabelSelector: propagatorMetricsSelector})
+			if err != nil {
+				return err
+			}
+			return len(routeList.Items)
+		}, defaultTimeoutSeconds, 1).Should(Or(Equal(0), Equal(1)))
+
+		if len(routeList.Items) == 0 {
+			By("Exposing the metrics service as a route")
+			_, err = oc("expose", "service", metricsSvc.Name, "-n", ocmNS, `--overrides={"spec":{"tls":{"termination":"reencrypt"}}}`)
+			Expect(err).To(BeNil())
+
+			Eventually(func() interface{} {
+				var err error
+				routeList, err = clientHubDynamic.Resource(common.GvrRoute).Namespace(ocmNS).List(context.TODO(), metav1.ListOptions{LabelSelector: propagatorMetricsSelector})
+				if err != nil {
+					return err
+				}
+				return len(routeList.Items)
+			}, defaultTimeoutSeconds, 1).Should(Equal(1))
+		}
+
+		routeHost = routeList.Items[0].Object["spec"].(map[string]interface{})["host"].(string)
+		By("Got the metrics route url: " + routeHost)
+	})
+	It("Checks that the endpoint does not expose metrics without auth", func() {
+		Eventually(func() interface{} {
+			_, status, err := getMetricsFromRoute("")
+			if err != nil {
+				return err
+			}
+			return status
+		}, defaultTimeoutSeconds, 1).Should(ContainSubstring("Unauthorized"))
+	})
+	It("Checks that endpoint has a HELP comment for the metric", func() {
+		By("Creating a policy")
+		oc("apply", "-f", compliantPolicyYaml, "-n", userNamespace, "--kubeconfig="+kubeconfigHub)
+		// Don't need to check compliance - just need to guarantee there is a policy in the cluster
+
+		token, err := oc("whoami", "-t")
+		Expect(err).To(BeNil())
+		Eventually(func() interface{} {
+			resp, _, err := getMetricsFromRoute(strings.TrimSpace(token))
+			if err != nil {
+				return err
+			}
+			return resp
+		}, defaultTimeoutSeconds, 1).Should(ContainSubstring("HELP " + metricName))
+	})
+	It("Checks that a compliant policy reports a metric of 0", func() {
+		By("Creating a compliant policy")
+		oc("apply", "-f", compliantPolicyYaml, "-n", userNamespace, "--kubeconfig="+kubeconfigHub)
+		Eventually(
+			getComplianceState(compliantPolicyName),
+			defaultTimeoutSeconds,
+			1,
+		).Should(Equal(policiesv1.Compliant))
+
+		By("Checking the policy metric")
+		token, err := oc("whoami", "-t")
+		Expect(err).To(BeNil())
+		policyLabel := `policy="` + compliantPolicyName + `"`
+		Eventually(func() interface{} {
+			resp, _, err := getMetricsFromRoute(strings.TrimSpace(token))
+			if err != nil {
+				return err
+			}
+			return resp
+		}, defaultTimeoutSeconds, 1).Should(matchMetricValue(metricName, policyLabel, "0"))
+	})
+	It("Checks that a noncompliant policy reports a metric of 1", func() {
+		By("Creating a noncompliant policy")
+		oc("apply", "-f", noncompliantPolicyYaml, "-n", userNamespace, "--kubeconfig="+kubeconfigHub)
+		Eventually(
+			getComplianceState(noncompliantPolicyName),
+			defaultTimeoutSeconds,
+			1,
+		).Should(Equal(policiesv1.NonCompliant))
+
+		By("Checking the policy metric")
+		token, err := oc("whoami", "-t")
+		Expect(err).To(BeNil())
+		policyLabel := `policy="` + noncompliantPolicyName + `"`
+		Eventually(func() interface{} {
+			resp, _, err := getMetricsFromRoute(strings.TrimSpace(token))
+			if err != nil {
+				return err
+			}
+			return resp
+		}, defaultTimeoutSeconds, 1).Should(matchMetricValue(metricName, policyLabel, "1"))
+	})
+	It("Cleans up", func() {
+		oc("delete", "-f", compliantPolicyYaml, "-n", userNamespace, "--kubeconfig="+kubeconfigHub)
+		oc("delete", "-f", noncompliantPolicyYaml, "-n", userNamespace, "--kubeconfig="+kubeconfigHub)
+		oc("delete", "route", "-n", ocmNS, "-l", propagatorMetricsSelector)
+	})
+})
+
+func oc(args ...string) (string, error) {
+	output, err := exec.Command("oc", args...).CombinedOutput()
+	if len(args) > 0 && args[0] != "whoami" {
+		fmt.Println(string(output))
+	}
+	return string(output), err
+}
+
+func getMetricsFromRoute(authToken string) (body, status string, err error) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+		Timeout: 5 * time.Second,
+	}
+	req, err := http.NewRequest("GET", "https://"+routeHost+"/metrics", nil)
+	if err != nil {
+		return "", "", err
+	}
+	if authToken != "" {
+		req.Header.Add("Authorization", "Bearer "+authToken)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", "", err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := io.ReadAll(resp.Body)
+	return string(bodyBytes), resp.Status, err
+}
+
+func matchMetricValue(name, label, value string) types.GomegaMatcher {
+	regex := `(?m)`              // multiline mode (makes ^ and $ work)
+	regex += "^" + name + "{"    // full name of metric at start of line
+	regex += ".*" + label + ".*" // label somewhere inside the {...}
+	regex += "} " + value + "$"  // value at the end of line
+	return MatchRegexp(regex)
+}
