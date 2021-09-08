576a6d7dad1ccb5a7beecc913e8b7fdf681d3600
Dale Haiducek <dhaiduce@redhat.com>
2021-09-08 15:56:24 -0400

    Add namespace cleanup
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

--- test/integration/policy_info_metric_test.go
+++ test/integration/policy_info_metric_test.go
@@ -157,5 +157,7 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test policy_governance_info metric
 		common.OcHub("delete", "route", "-n", ocmNS, "-l", propagatorMetricsSelector)
 		common.OcHub("delete", "clusterrolebinding", roleBindingName)
 		common.OcHub("delete", "serviceaccount", saName, "-n", userNamespace)
+		common.OcHub("delete", "namespace", userNamespace)
+		common.OcHub("delete", "namespace", "policy-metric-test-compliant")
 	})
 })
