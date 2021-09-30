4bd1bd9973ad5a21d8e2a2cbacb0801bb2f89b14
Yu Cao <ycao@redhat.com>
2021-09-30 13:51:01 -0400

    add comment
    Signed-off-by: Yu Cao <ycao@redhat.com>

--- test/policy-collection/policy_iam_test.go
+++ test/policy-collection/policy_iam_test.go
@@ -53,6 +53,8 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the stable IAM policy", func(
 
 	It("stable/"+iamPolicyName+" should be compliant", func() {
 		By("Checking if the status of the root policy is compliant")
+		// Increasing the time out for now to wait for the iam policy test from GRC-UI to complete.
+		// iam policy test from GRC-UI takes around 5 minutes to complete.
 		Eventually(getIAMComplianceState, defaultTimeoutSeconds*10, 1).Should(Equal(policiesv1.Compliant))
 	})
 
@@ -93,6 +95,8 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the stable IAM policy", func(
 
 	It("stable/"+iamPolicyName+" should be compliant", func() {
 		By("Checking if the status of the root policy is compliant")
+		// Increasing the time out for now to wait for the iam policy test from GRC-UI to complete.
+		// iam policy test from GRC-UI takes around 5 minutes to complete.
 		Eventually(getIAMComplianceState, defaultTimeoutSeconds*10, 1).Should(Equal(policiesv1.Compliant))
 	})
 
