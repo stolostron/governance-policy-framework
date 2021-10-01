2201535fcc4277704053e7cf4907f37bfd5b25ea
Dale Haiducek <dhaiduce@redhat.com>
2021-10-01 16:04:56 -0400

    Update e2e test with debug output
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -4,22 +4,20 @@
 
 set -e
 
-export DOCKER_IMAGE_AND_TAG=${1}
-
 if ! which kubectl > /dev/null; then
-    echo "installing kubectl"
+    echo "* Installing kubectl..."
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 fi
 if ! which kind > /dev/null; then
-    echo "installing kind"
+    echo "* Installing kind..."
     curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-$(uname)-amd64
     chmod +x ./kind
     sudo mv ./kind /usr/local/bin/kind
 fi
-echo "Installing ginkgo ..."
-go get github.com/onsi/ginkgo/ginkgo
-go get github.com/onsi/gomega/...
+echo "* Installing ginkgo ..."
+make e2e-dependencies
 
+echo "* Creating Kind cluster(s)..."
 export MANAGED_CLUSTER_NAME=managed
 make kind-create-cluster
 
@@ -27,7 +25,8 @@ make kind-create-cluster
 ./build/wait_for.sh pod -l k8s-app=kube-dns -n kube-system
 ./build/wait_for.sh pod -n local-path-storage
 
-make install-crds 
+echo "* Deploying policy framework..."
+make install-crds
 
 make install-resources
 
@@ -48,12 +47,20 @@ make kind-deploy-policy-controllers
 ./build/wait_for.sh pod -n olm
 ./build/wait_for.sh pod -n cert-manager
 
+echo "* Listing all pods on cluster..."
 kubectl get pods -A
 
-echo "all ready! start to test"
+echo "* All ready! Start to test..."
 
-make e2e-test
+make e2e-test || ERROR_CODE=$?
 
-echo "delete cluster"
+if [[ -n "${ERROR_CODE}" ]] && [[ -n "${ARTIFACT_DIR}" ]]; then
+    echo "* Detected test failure. Collecting debug logs..."
+    make e2e-debug
+fi
+
+echo "* Deleting Kind cluster..."
 make kind-delete-cluster 
 
+# Since we may have captured an exit code previously, manually exit with it here
+exit ${ERROR_CODE}
