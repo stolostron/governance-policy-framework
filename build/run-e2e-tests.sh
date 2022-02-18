2f774a21827d40b2401c714c203265e87f8ff153
Yu Cao <ycao@redhat.com>
2022-02-18 07:50:47 -0500

    Upgrade kind to v0.11.1
    Signed-off-by: Yu Cao <ycao@redhat.com>

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -16,8 +16,9 @@ if ! which kubectl > /dev/null; then
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 fi
 if ! which kind > /dev/null; then
-    echo "* Installing kind..."
-    curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-$(uname)-amd64
+    KIND_VERSION=${KIND_VERSION:-"v0.11.1"}   
+    echo "* Installing kind ${KIND_VERSION}..."
+    curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-$(uname)-amd64
     chmod +x ./kind
     sudo mv ./kind /usr/local/bin/kind
 fi
