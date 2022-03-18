792e9794dfeb7a0327d9a4777b54d86cb0cb39a4
mprahl <mprahl@users.noreply.github.com>
2022-03-18 13:39:19 -0400

    Add a test for the stable/policy-limitmemory policy
    Resolves:
    https://github.com/stolostron/backlog/issues/20844
    
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

---
+++ test/integration/policy-limitmemory_test.go
@@ -0,0 +1,125 @@
+// Copyright (c) 2022 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package integration
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	policiesv1 "github.com/stolostron/governance-policy-propagator/api/v1"
+	"github.com/stolostron/governance-policy-propagator/test/utils"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8stypes "k8s.io/apimachinery/pkg/types"
+
+	"github.com/stolostron/governance-policy-framework/test/common"
+)
+
+const policyLimitMemoryURL = "https://raw.githubusercontent.com/stolostron/policy-collection/main/stable/SC-System-and-Communications-Protection/policy-limitmemory.yaml"
+const policyLimitMemoryName = "policy-limitmemory"
+const limitRangeName = "mem-limit-range"
+
+var limitMemoryLabel = Label("policy-collection", "stable", "SVT")
+
+var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the "+policyLimitMemoryName+" policy", limitMemoryLabel, func() {
+	It("stable/"+policyLimitMemoryName+" should be created on the Hub", func() {
+		By("Creating the policy on the Hub")
+		_, err := utils.KubectlWithOutput(
+			"apply", "-f", policyLimitMemoryURL, "-n", userNamespace, "--kubeconfig="+kubeconfigHub,
+		)
+		Expect(err).To(BeNil())
+
+		By("Patching the namespaceSelector to use the " + userNamespace + " namespace")
+		_, err = clientHubDynamic.Resource(common.GvrPolicy).Namespace(userNamespace).Patch(
+			context.TODO(),
+			policyLimitMemoryName,
+			k8stypes.JSONPatchType,
+			[]byte(`[{"op": "replace", "path": "/spec/policy-templates/0/objectDefinition/spec/namespaceSelector/include", "value": ["`+userNamespace+`"]}]`),
+			metav1.PatchOptions{},
+		)
+		Expect(err).To(BeNil())
+
+		By("Patching placement rule")
+		err = common.PatchPlacementRule(
+			userNamespace, "placement-"+policyLimitMemoryName, clusterNamespace, kubeconfigHub,
+		)
+		Expect(err).To(BeNil())
+
+		By("Checking that " + policyLimitMemoryName + " exists on the Hub cluster")
+		rootPolicy := utils.GetWithTimeout(
+			clientHubDynamic, common.GvrPolicy, policyLimitMemoryName, userNamespace, true, defaultTimeoutSeconds,
+		)
+		Expect(rootPolicy).NotTo(BeNil())
+	})
+
+	It("stable/"+policyLimitMemoryName+" should be created on managed cluster", func() {
+		By("Checking the policy on managed cluster in ns " + clusterNamespace)
+		managedPolicy := utils.GetWithTimeout(
+			clientManagedDynamic,
+			common.GvrPolicy,
+			userNamespace+"."+policyLimitMemoryName,
+			clusterNamespace,
+			true,
+			defaultTimeoutSeconds,
+		)
+		Expect(managedPolicy).NotTo(BeNil())
+	})
+
+	It("stable/"+policyLimitMemoryName+" should be NonCompliant", func() {
+		By("Checking if the status of the root policy is NonCompliant")
+		Eventually(
+			common.GetComplianceState(clientHubDynamic, userNamespace, policyLimitMemoryName, clusterNamespace),
+			defaultTimeoutSeconds*2,
+			1,
+		).Should(Equal(policiesv1.NonCompliant))
+	})
+
+	It("Enforcing stable/"+policyLimitMemoryName, func() {
+		By("Patching remediationAction = enforce on the root policy")
+		_, err := clientHubDynamic.Resource(common.GvrPolicy).Namespace(userNamespace).Patch(
+			context.TODO(),
+			policyLimitMemoryName,
+			k8stypes.JSONPatchType,
+			[]byte(`[{"op": "replace", "path": "/spec/remediationAction", "value": "enforce"}]`),
+			metav1.PatchOptions{},
+		)
+		Expect(err).To(BeNil())
+	})
+
+	It("stable/"+policyLimitMemoryName+" should be Compliant", func() {
+		By("Checking if the status of the root policy is Compliant")
+		Eventually(
+			common.GetComplianceState(clientHubDynamic, userNamespace, policyLimitMemoryName, clusterNamespace),
+			defaultTimeoutSeconds*2,
+			1,
+		).Should(Equal(policiesv1.Compliant))
+	})
+
+	It("The LimitRange should exist", func() {
+		By("Checking the LimitRange in the " + userNamespace + " namespace")
+		Eventually(
+			func() error {
+				_, err := clientManaged.CoreV1().LimitRanges(userNamespace).Get(
+					context.TODO(), limitRangeName, metav1.GetOptions{},
+				)
+
+				return err
+			},
+			defaultTimeoutSeconds*2,
+			1,
+		).Should(BeNil())
+	})
+
+	It("Cleans up", func() {
+		_, err := utils.KubectlWithOutput(
+			"delete", "-f", policyLimitMemoryURL, "-n", userNamespace, "--kubeconfig="+kubeconfigHub,
+		)
+		Expect(err).To(BeNil())
+
+		err = clientManaged.CoreV1().LimitRanges(userNamespace).Delete(
+			context.TODO(), limitRangeName, metav1.DeleteOptions{},
+		)
+		Expect(err).To(BeNil())
+	})
+})
