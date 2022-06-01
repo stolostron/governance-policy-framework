4ec0939d765d9b6d2dd05e702434c4f157680cd6
Dale Haiducek <dhaiduce@redhat.com>
2022-06-01 15:50:10 -0400

    Adjust exit logic
    The EXIT_CODE wasn't being reset on success.
    
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

--- build/set-claim.sh
+++ build/set-claim.sh
@@ -37,8 +37,9 @@ if [ "${POWER_STATE}" = "Running" ]; then
   # Wait for ClusterClaim to be Running
   for i in {1..20}; do
     echo "Checking whether ClusterClaim ${CLAIM} is Running (${i}/10):"
-    oc wait --for=condition=ClusterRunning=True clusterclaims.hive/${CLAIM} --timeout 30s || EXIT_CODE=$?
-    if [ -z "${EXIT_CODE}" ]; then
+    oc wait --for=condition=ClusterRunning=True clusterclaims.hive/${CLAIM} --timeout 30s
+    EXIT_CODE=$?
+    if [[ "${EXIT_CODE}" == "0" ]]; then
       break
     fi
   done
