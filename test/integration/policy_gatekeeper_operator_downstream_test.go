9eaa48569995308b0802452838a8d3e8cd412970
Justin Kulikauskas <jkulikau@redhat.com>
2022-07-06 14:34:00 -0400

    Adjust timeouts over 7.5 minutes
    These timeouts are now set to `MaxTravisTimeoutSeconds` - just under 10
    minutes. This will prevent Travis from killing our build prematurely.
    It should also make it easier to find the longest timeouts for future
    adjustments - anything using the max is a target for test optimization.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- test/integration/policy_gatekeeper_operator_downstream_test.go
+++ test/integration/policy_gatekeeper_operator_downstream_test.go
@@ -307,7 +307,7 @@ var _ = Describe("RHACM4K-3055", Ordered, Label("policy-collection", "stable", "
 					return "0;0"
 				}
 				return fmt.Sprintf("%d;%d", len(podList.Items[0].Spec.Containers[0].Args), len(podList.Items[1].Spec.Containers[0].Args))
-			}, defaultTimeoutSeconds*15, 1).Should(Equal("7;7"))
+			}, common.MaxTravisTimeoutSeconds, 1).Should(Equal("7;7"))
 			Eventually(func() interface{} {
 				podList, _ := clientManaged.CoreV1().Pods("openshift-gatekeeper-system").List(context.TODO(), metav1.ListOptions{LabelSelector: "control-plane=controller-manager"})
 				// create a list to avoid hard-coding checking the order the arguments
@@ -389,7 +389,7 @@ var _ = Describe("RHACM4K-3055", Ordered, Label("policy-collection", "stable", "
 					return "0/0"
 				}
 				return fmt.Sprintf("%d/%d", len(podList.Items[0].Spec.Containers[0].Args), len(podList.Items[1].Spec.Containers[0].Args))
-			}, defaultTimeoutSeconds*15, 1).Should(Equal("6/6"))
+			}, common.MaxTravisTimeoutSeconds, 1).Should(Equal("6/6"))
 		})
 		It("Informing stable/policy-gatekeeper-operator", func() {
 			Eventually(func() interface{} {
