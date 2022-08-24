d4aaea3a9f6e0d85c354103592da1fa21561ae4d
Dale Haiducek <dhaiduce@redhat.com>
2022-08-24 09:47:24 -0400

    Delete the created ns to avoid pointless retries
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

--- test/integration/policy_gatekeeper_operator_downstream_test.go
+++ test/integration/policy_gatekeeper_operator_downstream_test.go
@@ -228,8 +228,12 @@ var _ = Describe("RHACM4K-3055", Ordered, Label("policy-collection", "stable", "
 			By("Creating invalid namespace on managed")
 			Eventually(func() interface{} {
 				out, _ := utils.KubectlWithOutput("apply", "-f", "../resources/gatekeeper/ns-create-invalid.yaml", "--kubeconfig="+kubeconfigManaged)
+				if out == "namespace/e2etestfail created" {
+					GinkgoWriter.Println("Deleting created namespace to retry create:")
+					_, _ = utils.KubectlWithOutput("delete", "-f", "../resources/gatekeeper/ns-create-invalid.yaml", "--kubeconfig="+kubeconfigManaged)
+				}
 				return out
-			}, defaultTimeoutSeconds*6, 1).Should(And(
+			}, defaultTimeoutSeconds*6, 5).Should(And(
 				ContainSubstring("validation.gatekeeper.sh"),
 				ContainSubstring("denied"),
 				ContainSubstring("ns-must-have-gk")))
