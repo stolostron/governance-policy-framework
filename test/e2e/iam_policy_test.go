b737a99d601e211f601c2fc3e433b09287faf334
Justin Kulikauskas <jkulikau@redhat.com>
2022-10-11 14:19:20 -0400

    Remove dynamic clients args from common functions
    Since the dynamic clients are now stored in the common package, they are
    not necessary as arguments to these functions. This prevents cases where
    the wrong client might be supplied to the functions on accident.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>
    (cherry picked from commit 37688cc4c717c2f33b75857f24a87d3d43cbe929)

--- test/e2e/iam_policy_test.go
+++ test/e2e/iam_policy_test.go
@@ -15,16 +15,16 @@ var _ = Describe("Test iam policy", func() {
 		const iamPolicyName string = "iam-policy"
 		const iamPolicyYaml string = "../resources/iam_policy/iam-policy.yaml"
 		It("should be created on managed cluster", func() {
-			common.DoCreatePolicyTest(clientHubDynamic, clientManagedDynamic, iamPolicyYaml, common.GvrIamPolicy)
+			common.DoCreatePolicyTest(iamPolicyYaml, common.GvrIamPolicy)
 		})
 		It("the policy should be compliant as there is no clusterrolebindings", func() {
-			common.DoRootComplianceTest(clientHubDynamic, iamPolicyName, policiesv1.Compliant)
+			common.DoRootComplianceTest(iamPolicyName, policiesv1.Compliant)
 		})
 		It("the policy should be noncompliant after creating clusterrolebindings", func() {
 			By("Creating ../resources/iam_policy/clusterrolebinding.yaml")
 			_, err := common.OcManaged("apply", "-f", "../resources/iam_policy/clusterrolebinding.yaml")
 			Expect(err).To(BeNil())
-			common.DoRootComplianceTest(clientHubDynamic, iamPolicyName, policiesv1.NonCompliant)
+			common.DoRootComplianceTest(iamPolicyName, policiesv1.NonCompliant)
 		})
 		It("the policy should be compliant again after delete clusterrolebindings", func() {
 			By("Deleting ../resources/iam_policy/clusterrolebinding.yaml")
@@ -34,10 +34,10 @@ var _ = Describe("Test iam policy", func() {
 				"--ignore-not-found",
 			)
 			Expect(err).To(BeNil())
-			common.DoRootComplianceTest(clientHubDynamic, iamPolicyName, policiesv1.Compliant)
+			common.DoRootComplianceTest(iamPolicyName, policiesv1.Compliant)
 		})
 		AfterAll(func() {
-			common.DoCleanupPolicy(clientHubDynamic, clientManagedDynamic, iamPolicyYaml, common.GvrIamPolicy)
+			common.DoCleanupPolicy(iamPolicyYaml, common.GvrIamPolicy)
 		})
 	})
 })
