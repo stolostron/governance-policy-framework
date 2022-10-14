6a72738b1f579ff1e0c67ff30371413907eaf960
Dale Haiducek <dhaiduce@redhat.com>
2022-10-14 12:37:03 -0400

    Add GitOps utils file
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

--- test/integration/policy_generator_test.go
+++ test/integration/policy_generator_test.go
@@ -4,117 +4,34 @@ package integration
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/stolostron/governance-policy-framework/test/common"
 )
 
 var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the Policy Generator "+
 	"in an App subscription", Ordered, Label("BVT"), func() {
+	const policyName = "e2e-grc-policy-app"
 	const namespace = "grc-e2e-policy-generator"
-	const secret = "grc-e2e-subscription-admin-user"
-	const subAdminBinding = "open-cluster-management:subscription-admin"
-	ocpUser := common.OCPUser{
-		ClusterRoles: []types.NamespacedName{
-			{Name: "open-cluster-management:admin:local-cluster"},
-			{
-				Name:      "admin",
-				Namespace: namespace,
-			},
-		},
-		ClusterRoleBindings: []string{subAdminBinding},
-		Password:            "",
-		Username:            "grc-e2e-subscription-admin",
-	}
+	const username = "grc-e2e-subadmin-user-polgen"
+	var ocpUser common.OCPUser
 
 	It("Sets up the application subscription", func() {
-		By("Verifying that the subscription-admin ClusterRoleBinding exists")
-		// Occasionally, the subscription-admin ClusterRoleBinding may not exist due to some unknown
-		// error. This ClusterRoleBinding is supposed to have been created by the App Lifecycle
-		// controllers. In this unusual case, create the ClusterRoleBinding based on the advice from
-		// the Application Lifecycle squad.
-		subAdminBindingObj := rbacv1.ClusterRoleBinding{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: subAdminBinding,
-			},
-			RoleRef: rbacv1.RoleRef{
-				APIGroup: "rbac.authorization.k8s.io",
-				Kind:     "ClusterRole",
-				Name:     subAdminBinding,
-			},
-		}
-		_, err := clientHub.RbacV1().ClusterRoleBindings().Create(
-			context.TODO(), &subAdminBindingObj, metav1.CreateOptions{},
-		)
-		if err != nil {
-			Expect(k8serrors.IsAlreadyExists(err)).Should(
-				BeTrue(),
-				"Expected error to be 'already exists': "+fmt.Sprint(err),
-			)
-		}
-
-		By("Cleaning up any existing subscription-admin user config")
-		cleanup(namespace, secret, ocpUser)
-
-		By("Creating a subscription-admin user and configuring IDP")
-		// Create a namespace to house the subscription configuration.
-		nsObj := corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
-		_, err = clientHub.CoreV1().Namespaces().Create(
-			context.TODO(), &nsObj, metav1.CreateOptions{},
-		)
-		Expect(err).Should(BeNil())
-
-		// Create a subscription and local-cluster administrator OpenShift user that can be used
-		// for logging in.
-		userPassword, err := common.GenerateInsecurePassword()
-		Expect(err).Should(BeNil())
-		ocpUser.Password = userPassword
-		err = common.CreateOCPUser(clientHub, clientHubDynamic, secret, ocpUser)
-		Expect(err).Should(BeNil())
-
-		// Get a kubeconfig logged in as the subscription and local-cluster administrator OpenShift
-		// user.
-		hubServerURL, err := common.OcHub("whoami", "--show-server=true")
-		Expect(err).Should(BeNil())
-		hubServerURL = strings.TrimSuffix(hubServerURL, "\n")
-		// Use eventually since it can take a while for OpenShift to configure itself with the new
-		// identity provider (IDP).
-		var kubeconfigSubAdmin string
-		const fiveMinutes = 5 * 60
-		Eventually(
-			func() error {
-				var err error
-				kubeconfigSubAdmin, err = common.GetKubeConfig(
-					hubServerURL, ocpUser.Username, ocpUser.Password,
-				)
-
-				return err
-			},
-			fiveMinutes,
-			10,
-		).Should(BeNil())
-		// Delete the kubeconfig file after the test.
-		defer func() { os.Remove(kubeconfigSubAdmin) }()
+		By("Creating and setting up the GitOps user")
+		ocpUser = common.GitOpsUserSetup(namespace, username)
 
 		By("Creating the application subscription")
-		_, err = common.OcHub(
+		_, err := common.OcUser(
+			ocpUser,
 			"apply",
 			"-f",
 			"../resources/policy_generator/subscription.yaml",
 			"-n",
 			namespace,
-			"--kubeconfig="+kubeconfigSubAdmin,
 		)
 		Expect(err).Should(BeNil())
 	})
@@ -143,7 +60,7 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the Policy Generator "+
 		Expect(err).Should(BeNil())
 		Expect(found).Should(BeTrue())
 		Expect(len(policies)).Should(Equal(1))
-		Expect(policies[0]).Should(Equal("e2e-grc-policy-app"))
+		Expect(policies[0]).Should(Equal(policyName))
 
 		By("Checking that the root policy was created")
 		policyRsrc := clientHubDynamic.Resource(common.GvrPolicy)
@@ -152,7 +69,7 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the Policy Generator "+
 			func() error {
 				var err error
 				policy, err = policyRsrc.Namespace(namespace).Get(
-					context.TODO(), "e2e-grc-policy-app", metav1.GetOptions{},
+					context.TODO(), policyName, metav1.GetOptions{},
 				)
 
 				return err
@@ -182,7 +99,7 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the Policy Generator "+
 				var err error
 				policy, err = policyRsrc.Namespace("local-cluster").Get(
 					context.TODO(),
-					"grc-e2e-policy-generator.e2e-grc-policy-app",
+					namespace+"."+policyName,
 					metav1.GetOptions{},
 				)
 
@@ -198,7 +115,7 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the Policy Generator "+
 			func() error {
 				var err error
 				policy, err = configPolicyRsrc.Namespace("local-cluster").Get(
-					context.TODO(), "e2e-grc-policy-app", metav1.GetOptions{},
+					context.TODO(), policyName, metav1.GetOptions{},
 				)
 
 				return err
@@ -210,6 +127,6 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the Policy Generator "+
 
 	AfterAll(func() {
 		By("Cleaning up the changes made to the cluster in the test")
-		cleanup(namespace, secret, ocpUser)
+		common.GitOpsCleanup(namespace, ocpUser)
 	})
 })
