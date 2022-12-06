b82ad7d96c0cf5283d1a9310fc53f1abf3ef6f19
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2022-12-06 10:39:20 -0500

    Update timeout to match other timeouts
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- test/integration/policy_generator_acm_hardening_test.go
+++ test/integration/policy_generator_acm_hardening_test.go
@@ -170,7 +170,7 @@ var _ = Describe("GRC: [P1][Sev1][policy-grc] Test the ACM Hardening "+
 
 				return err
 			},
-			defaultTimeoutSeconds,
+			defaultTimeoutSeconds*2,
 			1,
 		).Should(BeNil())
 	})
