e7cbda3420b9815f7907ed73e74e8d90c12586bf
mprahl <mprahl@users.noreply.github.com>
2023-01-23 12:13:24 -0500

    Use a local path for the oc CLI in TravisCI
    This fixes a permission error.
    
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

--- build/download-clis.sh
+++ build/download-clis.sh
@@ -2,17 +2,17 @@
 # Copyright (c) 2020 Red Hat, Inc.
 # Copyright Contributors to the Open Cluster Management project
 
-
-
 set -e
 
+CLI_DESTINATION_DIR="${CLI_DESTINATION_DIR:=/usr/local/bin}"
+
 if ! which oc > /dev/null; then
-    echo "Installing oc and kubectl clis..."
+    echo "Installing oc and kubectl clis in $CLI_DESTINATION_DIR..."
     mkdir clis-unpacked
     curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.10.21/openshift-client-linux.tar.gz
     tar -xzf oc.tar.gz -C clis-unpacked
     chmod 755 ./clis-unpacked/oc
     chmod 755 ./clis-unpacked/kubectl
-    mv ./clis-unpacked/oc /usr/local/bin/oc
-    mv ./clis-unpacked/kubectl /usr/local/bin/kubectl
+    mv ./clis-unpacked/oc "$CLI_DESTINATION_DIR/oc"
+    mv ./clis-unpacked/kubectl "$CLI_DESTINATION_DIR/kubectl"
 fi
