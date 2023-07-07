d77d9dc9b33665ca9d895ffb843cf9d088106b78
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2023-07-07 12:54:16 -0400

    Clean up namespaces in AfterSuite
    The namespaces are being created in the BeforeSuite but then are
    cleaned up in each individual test. Moving cleanup to the AfterSuite
    could help the flakiness but also makes sense overall.
    
    ref: https://issues.redhat.com/browse/ACM-5966
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- test/common/gitops_utils.go
+++ test/common/gitops_utils.go
@@ -17,6 +17,12 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
+var gitopsTestNamespaces = []string{
+	"grc-e2e-policy-generator",
+	"grc-e2e-remote-policy-generator",
+	"policies",
+}
+
 // GitOpsUserSetup configures a new user to use for the GitOps tests. It updates the provided
 // OCPUser instance, which contains a path to the created kubeconfig file.
 func GitOpsUserSetup(ocpUser *OCPUser) {
@@ -29,12 +35,6 @@ func GitOpsUserSetup(ocpUser *OCPUser) {
 	}
 	ocpUser.ClusterRoleBindings = []string{subAdminBinding}
 	ocpUser.Username = "grc-e2e-subadmin-user"
-
-	gitopsTestNamespaces := []string{
-		"grc-e2e-policy-generator",
-		"grc-e2e-remote-policy-generator",
-		"policies",
-	}
 
 	// Add additional cluster roles for each namespace
 	for _, ns := range gitopsTestNamespaces {
@@ -164,5 +164,9 @@ func GitOpsCleanup(user OCPUser) {
 	err = ClientHub.CoreV1().Secrets("openshift-config").Delete(context.TODO(), user.Username, metav1.DeleteOptions{})
 	if !k8serrors.IsNotFound(err) {
 		ExpectWithOffset(1, err).ShouldNot(HaveOccurred())
+	}
+
+	for _, ns := range gitopsTestNamespaces {
+		CleanupHubNamespace(ns)
 	}
 }
