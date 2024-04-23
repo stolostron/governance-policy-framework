006ed0a7af315dbd2204da2ac11d9e47864776e5
mprahl <mprahl@users.noreply.github.com>
2024-04-23 10:16:01 -0400

    Use the release-2.10 branch for the policy-collection
    2.11+ will be dynamic.
    
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

--- test/integration/integration_suite_test.go
+++ test/integration/integration_suite_test.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	policyCollectBaseURL      = "https://raw.githubusercontent.com/stolostron/policy-collection/main/"
+	policyCollectBaseURL      = "https://raw.githubusercontent.com/stolostron/policy-collection/release-2.10/"
 	policyCollectCommunityURL = policyCollectBaseURL + "community/"
 	policyCollectStableURL    = policyCollectBaseURL + "stable/"
 	policyCollectACURL        = policyCollectStableURL + "AC-Access-Control/"
