a13a8704849232976fd56abec1c5aa1a1e124f64
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2024-10-09 10:59:12 -0400

    Install `oc` and bump to `stable` version
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/download-clis.sh
+++ build/download-clis.sh
@@ -7,14 +7,14 @@ set -e
 CLI_DESTINATION_DIR="${CLI_DESTINATION_DIR:=/usr/local/bin}"
 
 if ! which oc > /dev/null; then
-    echo "Installing oc and kubectl clis in $CLI_DESTINATION_DIR..."
+    echo "Installing oc and kubectl CLIs to ${CLI_DESTINATION_DIR}..."
     mkdir clis-unpacked
-    curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.10.21/openshift-client-linux.tar.gz
+    curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/openshift-client-linux.tar.gz
     tar -xzf oc.tar.gz -C clis-unpacked
     chmod 755 ./clis-unpacked/oc
     chmod 755 ./clis-unpacked/kubectl
-    mv ./clis-unpacked/oc "$CLI_DESTINATION_DIR/oc"
-    mv ./clis-unpacked/kubectl "$CLI_DESTINATION_DIR/kubectl"
+    mv ./clis-unpacked/oc "${CLI_DESTINATION_DIR}/oc"
+    mv ./clis-unpacked/kubectl "${CLI_DESTINATION_DIR}/kubectl"
     rm -rf ./clis-unpacked
     rm -f oc.tar.gz
 fi
