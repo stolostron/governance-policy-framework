f8df0bf98536d7cfef2146dfeff260ad46c2f5a8
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2024-10-17 16:10:46 -0400

    Update upstream release script with instructions
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/main-branch-sync/upstream-release.sh
+++ build/main-branch-sync/upstream-release.sh
@@ -20,7 +20,22 @@ for REPO in ${REPOS}; do
   ${GIT} push origin ${NEW_RELEASE}
 done
 
-echo "=== View the new releases"
+echo "=== Review and submit the new releases (GitHub Actions may still be running to create them)"
 for REPO in ${REPOS}; do
   echo "* https://github.com/${REPO}/releases/tag/${NEW_RELEASE}"
 done
+
+echo
+echo '=== Next steps
+* Create a new release for the policy generator if needed:
+  - https://github.com/open-cluster-management-io/policy-generator-plugin
+
+* If a new generator release was needed, update the POLICY_GENERATOR_TAG variable in all applicable AppSub Dockerfiles:
+  - https://github.com/open-cluster-management-io/multicloud-operators-subscription/tree/main/build
+
+* Verify the propagator and addon-controller manifests in clusteradm:
+  - https://github.com/open-cluster-management-io/clusteradm/tree/main/pkg/cmd/install/hubaddon/scenario/addon/policy
+  
+  Reference manifests:
+  - https://github.com/open-cluster-management-io/governance-policy-propagator/tree/main/deploy
+  - https://github.com/open-cluster-management-io/governance-policy-addon-controller/tree/main/config'
